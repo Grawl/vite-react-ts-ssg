41589723c5651482efe871271e9a07d01d60c219
Даниил Пронин <mail@grawl.ru>
2022-11-30 17:46:56 +1000

    use more Helmet data

--- template.js
+++ template.js
@@ -5,16 +5,32 @@ import dedent from 'dedent-js'
 export const appTemplate = (vite, app) => {
 	const helmet = Helmet.renderStatic()
 	const attributes = s => s ? ` ${s}` : ''
+	const {
+		htmlAttributes,
+		bodyAttributes,
+		// titleAttributes,
+		title,
+		meta,
+		link,
+		noscript,
+		base,
+		script,
+		style,
+	} = helmet
 	return dedent`
 		<!doctype html>
-		<html${attributes(helmet.htmlAttributes.toString())}>
+		<html${attributes(htmlAttributes.toString())}>
 			<head>
-				${helmet.title.toString()}
-				${helmet.meta.toString()}
-				${helmet.link.toString()}
+				${base.toString()}
+				${title.toString()}
+				${meta.toString()}
+				${link.toString()}
+				${noscript.toString()}
+				${script.toString()}
+				${style.toString()}
 				${vite}
 			</head>
-			<body${attributes(helmet.bodyAttributes.toString())}>
+			<body${attributes(bodyAttributes.toString())}>
 				<div id='root'>${app}</div>
 			</body>
 		</html>
