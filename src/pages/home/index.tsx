a472715d6788cd34418390744f34b2a293b24a33
Даниил Пронин <mail@grawl.ru>
2022-11-30 18:21:42 +1000

    add runtime example

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { PageHead } from '../../layouts/components/head'
 import styles from './style.module.css'
 
 export const Home = () => {
+	const [count, setCount] = useState(0)
 	return (
 		<div className='page'>
 			<PageHead title='Home' titleTemplate='%s' />
 			<h1>Home</h1>
 			<p>Home page text</p>
-			<p className={styles.icon}>⚙️</p>
+			<p>You clicked {count} times</p>
+			<button
+				className={styles.button}
+				onClick={() => setCount(count + 1)}
+			>
+				Click me
+			</button>
 		</div>
 	)
 }
